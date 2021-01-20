1680098828f858b9b70297c6ac54958f4d6ead57
Kevin Rangel Moreira <rangel.moreira@outlook.com>
2021-01-20 15:15:21 -0300

    Category resource

---
+++ backend/src/main/java/com/devsuperior/dscatalog/resources/CategoryResource.java
@@ -0,0 +1,25 @@
+package com.devsuperior.dscatalog.resources;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.devsuperior.dscatalog.entities.Category;
+
+@RestController
+@RequestMapping(value = "/categories")
+public class CategoryResource {
+	
+	@GetMapping
+	public ResponseEntity<List<Category>> findAll(){
+		List<Category> list = new ArrayList<>();
+		list.add(new Category(1L, "Books"));
+		list.add(new Category(2L, "Eletronics"));
+		return ResponseEntity.ok().body(list);
+		
+	}
+}
