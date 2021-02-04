68a5942196fc242c773b37fd1f2c4618f00dace8
Kevin Rangel Moreira <rangel.moreira@outlook.com>
2021-02-04 18:44:45 -0300

    Backend adjustments

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -54,23 +54,3 @@ INSERT INTO tb_product_category (product_id, category_id) VALUES (22, 3);
 INSERT INTO tb_product_category (product_id, category_id) VALUES (23, 3);
 INSERT INTO tb_product_category (product_id, category_id) VALUES (24, 3);
 INSERT INTO tb_product_category (product_id, category_id) VALUES (25, 3);
-
-/*customers*/
-
-INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Ana Silva', '100.872.690-74', 2000, TIMESTAMP WITH TIME ZONE '1994-07-08T10:30:00Z',2);
-INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Ana Santos', '074.601.310-83', 3000, TIMESTAMP WITH TIME ZONE '1994-05-17T10:30:00Z',1);
-INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Rui Santos', '527.659.070-21', 5000, TIMESTAMP WITH TIME ZONE '1967-02-05T10:30:00Z',3);
-INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Pedro Alves', '251.686.770-02', 8000,  TIMESTAMP WITH TIME ZONE '1953-01-30T10:30:00Z',4);
-INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Nadir Goes', '272.141.210-85', 4000,  TIMESTAMP WITH TIME ZONE '1949-01-01T10:30:00Z',1);
-
-INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('José Silva', '715.409.450-81', 2500, TIMESTAMP WITH TIME ZONE '1991-02-08T10:30:00Z',2);
-INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Marta Santos', '093.625.770-97', 3200, TIMESTAMP WITH TIME ZONE '1994-08-19T10:30:00Z',1);
-INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Carlos Santos', '765.246.890-62', 4000, TIMESTAMP WITH TIME ZONE '1967-01-12T10:30:00Z',3);
-INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Mauro Alves', '805.605.750-17', 6500,  TIMESTAMP WITH TIME ZONE '1951-01-20T10:30:00Z',4);
-INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Paulo Goes', '208.501.930-79', 4100,  TIMESTAMP WITH TIME ZONE '1949-12-12T10:30:00Z',1);
-
-INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Claudia Bento', '144.082.490-84', 2000,  TIMESTAMP WITH TIME ZONE '1969-12-12T10:30:00Z',3);
-INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Roger Moreira', '133.804.560-12', 8000,  TIMESTAMP WITH TIME ZONE '1967-09-09T10:30:00Z',1);
-
-
-
