be887bd9ab4ebe70395cb1554c214a00e1ccdca2
Kevin Rangel Moreira <rangel.moreira@outlook.com>
2021-04-01 12:32:33 -0300

    H2 security configuration

--- backend/src/main/java/com/devsuperior/dscatalog/config/ResourceServerConfig.java
+++ backend/src/main/java/com/devsuperior/dscatalog/config/ResourceServerConfig.java
@@ -1,7 +1,10 @@
 package com.devsuperior.dscatalog.config;
 
+import java.util.Arrays;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
@@ -12,10 +15,14 @@ import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 @Configuration
 @EnableResourceServer
 public class ResourceServerConfig  extends ResourceServerConfigurerAdapter{
+	
+	@Autowired
+	private Environment env;//importação do spring
+	
 	@Autowired
 	private JwtTokenStore tokenStore;
 	
-	private static final String[] PUBLIC = {"/oauth/token"};
+	private static final String[] PUBLIC = {"/oauth/token","/h2-console/**"};
 	
 	private static final String[] OPERATOR_OR_ADMIN = {"/products/**", "/categories/**"}; 
 	
@@ -28,6 +35,11 @@ public class ResourceServerConfig  extends ResourceServerConfigurerAdapter{
 
 	@Override
 	public void configure(HttpSecurity http) throws Exception {
+		
+		//Libera o H2
+		if (Arrays.asList(env.getActiveProfiles()).contains("test")){
+			http.headers().frameOptions().disable();
+		}
 		
 		http.authorizeRequests()
 		.antMatchers(PUBLIC).permitAll()
