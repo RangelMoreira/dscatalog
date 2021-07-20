7c85bab8f33b8e48bc9a6d1f1f81e7aa6c7bfa98
Kevin Rangel Moreira <rangel.moreira@outlook.com>
2021-07-20 16:11:19 -0300

    Add env vars

--- front-web/src/core/utils/auth.ts
+++ front-web/src/core/utils/auth.ts
@@ -1,8 +1,8 @@
 import jwtDecode from 'jwt-decode';
 import history from './history';
 
-export const CLIENT_ID = 'dscatalog';
-export const CLIENT_SECRET = 'dscatalog123';
+export const CLIENT_ID = process.env.REACT_APP_CLIENT_ID ?? 'dscatalog';
+export const CLIENT_SECRET = process.env.REACT_APP_CLIENT_SECRET ?? 'dscatalog123';
 
 type LoginResponse = {
   access_token: string;
--- front-web/src/core/utils/request.ts
+++ front-web/src/core/utils/request.ts
@@ -8,19 +8,16 @@ type LoginData = {
   password: String;
 }
 
-const BASE_URL = 'http://localhost:8080';
+const BASE_URL = process.env.REACT_APP_BACKEND_URL ?? 'http://localhost:8080';
 
 axios.interceptors.response.use(function (response) {
   return response;
 }, function (error) {
   if (error.response.status === 401) {
     logout();
-
   }
-
   return Promise.reject(error);
 });
-
 
 export const makeRequest = (params: AxiosRequestConfig) => {
   return axios({
